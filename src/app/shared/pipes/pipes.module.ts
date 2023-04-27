7dfc34c796058378ec6cb33cdbece9b581d327c8
Mauro Gauto <mauro.gauto@globallogic.com>
2023-04-27 12:14:20 -0300

    add multi select for add courses and create new pipe ellipsis

--- src/app/shared/pipes/pipes.module.ts
+++ src/app/shared/pipes/pipes.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { FullNamePipe } from './full-name.pipe';
 import { ControlErrorMessagesPipe } from './control-error-messages.pipe';
+import { EllipsisPipe } from './ellipsis.pipe';
 
 @NgModule({
   declarations: [
     FullNamePipe,
-    ControlErrorMessagesPipe
+    ControlErrorMessagesPipe,
+    EllipsisPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
     FullNamePipe,
-    ControlErrorMessagesPipe
+    ControlErrorMessagesPipe,
+    EllipsisPipe
   ]
 })
 
